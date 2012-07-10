1e8c14f7fbc70ca378281f193412c0e69c036508
Richard Goulter <richard.goulter@gmail.com>
2012-07-10 13:14:02 +1200

    Replaced tab indentation with spaces.

--- GameEngine.js
+++ GameEngine.js
@@ -4,322 +4,322 @@ var KeyCodes = {LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40, CTRL: 17};
 * An object to look after game animation/movement.
 */
 function GameEngine(canvas){
-	var GE = {};
+  var GE = {};
 
-	var context;
-	var keysPressed = [];
-	var ships = [];
-	var movable = [];
-	var renderable = [];
-	var player = null;
-	var gamePaused = true;
-
-
-
-	/*
-	 Sets up the GameEngine with the document and supplied canvas.
-	*/
-	var init = function(){
-	  setCanvas(canvas);
-	  
-	  document.onkeyup = keyup;
-	  document.onkeydown = keydown;
-	};
+  var context;
+  var keysPressed = [];
+  var ships = [];
+  var movable = [];
+  var renderable = [];
+  var player = null;
+  var gamePaused = true;
 
 
 
-	var setCanvas = function(c){
-	  canvas = c;
-	  context = c.getContext("2d");
-	};
+  /*
+   Sets up the GameEngine with the document and supplied canvas.
+  */
+  var init = function(){
+    setCanvas(canvas);
+    
+    document.onkeyup = keyup;
+    document.onkeydown = keydown;
+  };
 
 
 
-	var clearWorld = function(){
-	  player =  null;
-	  ships = [];
-	  movable = [];
-	  renderable = [];
-	}
+  var setCanvas = function(c){
+    canvas = c;
+    context = c.getContext("2d");
+  };
 
 
 
-	// Sets up a game world.
-	var newGame = function(){
-	  clearWorld();
-	  
-	  var ship = new SpaceShip(null, {x: canvas.width / 2, y: canvas.height / 2})
-	  ships[0] = ship;
-	  player = ship;
-	  movable.push(player);
-	  renderable.push(player);
-	  
-	  addAsteroids(4);
-	};
+  var clearWorld = function(){
+    player =  null;
+    ships = [];
+    movable = [];
+    renderable = [];
+  }
 
 
 
-	//Adds n asteroids to the game
-	var addAsteroids = function(n){
-	  for(var i = 0; i < n; i++){
-		var args = {};
-		
-		do {
-		  //Set x and y, but not too close to centre
-		  args.x = Math.random() * canvas.width;
-		  args.y = Math.random() * canvas.width;
-		} while(Math.abs(canvas.width / 2 - args.x) < 20 &&
-				Math.abs(canvas.height / 2 - args.y) < 20);
-		
-		args.rockSize = Math.floor(Math.random() * 3) + 8;
-		var aster = new Asteroid(null, args);
-		
-		movable.push(aster);
-		renderable.push(aster);
-	  }
-	};
+  // Sets up a game world.
+  var newGame = function(){
+    clearWorld();
+    
+    var ship = new SpaceShip(null, {x: canvas.width / 2, y: canvas.height / 2})
+    ships[0] = ship;
+    player = ship;
+    movable.push(player);
+    renderable.push(player);
+    
+    addAsteroids(4);
+  };
+
+
+
+  //Adds n asteroids to the game
+  var addAsteroids = function(n){
+    for(var i = 0; i < n; i++){
+      var args = {};
+      
+      do {
+        //Set x and y, but not too close to centre
+        args.x = Math.random() * canvas.width;
+        args.y = Math.random() * canvas.width;
+      } while(Math.abs(canvas.width / 2 - args.x) < 20 &&
+              Math.abs(canvas.height / 2 - args.y) < 20);
+      
+      args.rockSize = Math.floor(Math.random() * 3) + 8;
+      var aster = new Asteroid(null, args);
+      
+      movable.push(aster);
+      renderable.push(aster);
+    }
+  };
 
 
 
 
-	// Start/Resume game animation.
-	var start = function(){
-	  if(gamePaused){
-		gamePaused = false;
-		var date = new Date();
-		var time = date.getTime();
-		tick(time);
-	  }
-	};
+  // Start/Resume game animation.
+  var start = function(){
+    if(gamePaused){
+      gamePaused = false;
+      var date = new Date();
+      var time = date.getTime();
+      tick(time);
+    }
+  };
 
 
 
-	var tick = function tick(lastTime){
-	  var date = new Date();
-	  var time = date.getTime();
-	  var delta = time - lastTime;
-	  
-	  move(delta);
-	  render();
-	  
-	  //Call function again
-	  if(!gamePaused){
-		window.setTimeout(tick, 1000/30, time);
-	  }
-	};
+  var tick = function tick(lastTime){
+    var date = new Date();
+    var time = date.getTime();
+    var delta = time - lastTime;
+    
+    move(delta);
+    render();
+    
+    //Call function again
+    if(!gamePaused){
+      window.setTimeout(tick, 1000/30, time);
+    }
+  };
 
 
 
-	/*
-	 Move game entities, and check collisions.
-	 O(n^2)
-	*/
-	var move = function(delta){
-	  handlePlayerInput();
-	  
-	  for(var i = 0; i < movable.length; i++){
-		if(movable[i]){
-		  var mover = movable[i];
-		  
-		  mover.move(delta);
-		  ensureWithinBounds(mover);
-		  
-		  if(mover.collidesWith){
-			for(var j = 0; j < movable.length; j++){
-			  if(i === j) continue;
-			  
-			  var other = movable[j];
-			  
-			  if(mover.collidesWith(other)){
-				handleCollision(mover, other);
-			  }
-			}
-		  }
-		  
-		  //Check if items have expired.
-		  if(mover.hasExpired && mover.hasExpired()){
-			removeObject(mover);
-		  }
-		}
-	  }
-	};
+  /*
+   Move game entities, and check collisions.
+   O(n^2)
+  */
+  var move = function(delta){
+    handlePlayerInput();
+    
+    for(var i = 0; i < movable.length; i++){
+      if(movable[i]){
+        var mover = movable[i];
+        
+        mover.move(delta);
+        ensureWithinBounds(mover);
+        
+        if(mover.collidesWith){
+          for(var j = 0; j < movable.length; j++){
+            if(i === j) continue;
+            
+            var other = movable[j];
+            
+            if(mover.collidesWith(other)){
+              handleCollision(mover, other);
+            }
+          }
+        }
+        
+        //Check if items have expired.
+        if(mover.hasExpired && mover.hasExpired()){
+          removeObject(mover);
+        }
+      }
+    }
+  };
 
 
 
-	var handleCollision = function(obj1, obj2){
-	  if(obj1.getObjectType() === ObjectTypes.SHIP && obj2.getObjectType() === ObjectTypes.ASTEROID ||
-		 obj1.getObjectType() === ObjectTypes.ASTEROID && obj2.getObjectType() === ObjectTypes.SHIP){
-		destroyObject(obj1);
-		destroyObject(obj2);
-	  }
-	  
-	  if((obj1.getObjectType() === ObjectTypes.BULLET && obj2.getObjectType() === ObjectTypes.ASTEROID) ||
-		 (obj1.getObjectType() === ObjectTypes.ASTEROID && obj2.getObjectType() === ObjectTypes.BULLET)){
-		destroyObject(obj1);
-		destroyObject(obj2);
-	  }
-	};
+  var handleCollision = function(obj1, obj2){
+    if((obj1.getObjectType() === ObjectTypes.SHIP && obj2.getObjectType() === ObjectTypes.ASTEROID) ||
+       (obj1.getObjectType() === ObjectTypes.ASTEROID && obj2.getObjectType() === ObjectTypes.SHIP)){
+      destroyObject(obj1);
+      destroyObject(obj2);
+    }
+    
+    if((obj1.getObjectType() === ObjectTypes.BULLET && obj2.getObjectType() === ObjectTypes.ASTEROID) ||
+       (obj1.getObjectType() === ObjectTypes.ASTEROID && obj2.getObjectType() === ObjectTypes.BULLET)){
+      destroyObject(obj1);
+      destroyObject(obj2);
+    }
+  };
 
 
 
-	// Creates an explosion as well as removing it
-	var destroyObject = function(movingObj){
-	  removeObject(movingObj);
-	  
-	  if(movingObj.getObjectType() === ObjectTypes.ASTEROID){
-		// Create Explosion
-		var expl = new Explosion(null, {movingParent: movingObj, numParticles: 12, maxRadius: 70});
-		movable.push(expl);
-		renderable.push(expl);
-		
-		// Create more asteroids
-		if(movingObj.getRockSize() > 7){
-		  createAsteroidsFromAsteroid(movingObj);
-		}
-	  } else if(movingObj.getObjectType() === ObjectTypes.SHIP){
-		var expl = new Explosion(null, {movingParent: movingObj, numParticles: 24, maxRadius: 100});
-		movable.push(expl);
-		renderable.push(expl);
-	  }
-	}
+  // Creates an explosion as well as removing it
+  var destroyObject = function(movingObj){
+    removeObject(movingObj);
+    
+    if(movingObj.getObjectType() === ObjectTypes.ASTEROID){
+      // Create Explosion
+      var expl = new Explosion(null, {movingParent: movingObj, numParticles: 12, maxRadius: 70});
+      movable.push(expl);
+      renderable.push(expl);
+      
+      // Create more asteroids
+      if(movingObj.getRockSize() > 7){
+        createAsteroidsFromAsteroid(movingObj);
+      }
+    } else if(movingObj.getObjectType() === ObjectTypes.SHIP){
+      var expl = new Explosion(null, {movingParent: movingObj, numParticles: 24, maxRadius: 100});
+      movable.push(expl);
+      renderable.push(expl);
+    }
+  }
 
 
 
-	var createAsteroidsFromAsteroid = function(movingObj){
-	  var x = movingObj.getX();
-	  var y = movingObj.getY();
-	  
-	  var n = Math.floor(Math.random() * 3) + 1;
-	  for(var i = 0; i < n; i++){
-		args = {};
-		args.x = x;
-		args.y = y;
-		args.angle = 2 * Math.PI * Math.random();
-		args.speed = (1 + 2 * Math.random()) * movingObj.getSpeed() / 3;
-		args.rockSize = Math.floor(Math.random() * 2) + 5;
-		
-		var aster = new Asteroid(null, args);
-		movable.push(aster);
-		renderable.push(aster);
-	  }
-	};
+  var createAsteroidsFromAsteroid = function(movingObj){
+    var x = movingObj.getX();
+    var y = movingObj.getY();
+    
+    var n = Math.floor(Math.random() * 3) + 1;
+    for(var i = 0; i < n; i++){
+      args = {};
+      args.x = x;
+      args.y = y;
+      args.angle = 2 * Math.PI * Math.random();
+      args.speed = (1 + 2 * Math.random()) * movingObj.getSpeed() / 3;
+      args.rockSize = Math.floor(Math.random() * 2) + 5;
+      
+      var aster = new Asteroid(null, args);
+      movable.push(aster);
+      renderable.push(aster);
+    }
+  };
 
 
 
-	var removeObject = function(movingObj){
-	  if(!movingObj.equals){
-		throw "Expects object to have equals functionality.";
-	  }
-	  
-	  function removeFromList(obj, list){
-		for(var i = 0; i < list.length; i++){
-		  if(obj.equals(list[i])){
-			list.splice(i, 1);
-			return;
-		  }
-		}
-	  }
-	  
-	  removeFromList(movingObj, movable);
-	  removeFromList(movingObj, renderable);
-	}
+  var removeObject = function(movingObj){
+    if(!movingObj.equals){
+      throw "Expects object to have equals functionality.";
+    }
+    
+    function removeFromList(obj, list){
+      for(var i = 0; i < list.length; i++){
+        if(obj.equals(list[i])){
+          list.splice(i, 1);
+          return;
+        }
+      }
+    }
+    
+    removeFromList(movingObj, movable);
+    removeFromList(movingObj, renderable);
+  }
 
 
 
-	// Wrap around world
-	var ensureWithinBounds = function(obj){
-	  var x = obj.getX();
-	  var y = obj.getY();
-	  var prevX = obj.getPreviousX();
-	  var prevY = obj.getPreviousY();
-	  
-	  if(x < 0){
-		obj.setX(prevX + canvas.width);
-		obj.setX(x + canvas.width);
-	  }
-	  if(x > canvas.width){
-		obj.setX(prevX - canvas.width);
-		obj.setX(x - canvas.width);
-	  }
-	  if(y < 0){
-		obj.setY(prevY + canvas.height);
-		obj.setY(y + canvas.height);
-	  }
-	  if(y > canvas.height){
-		obj.setY(prevY - canvas.height);
-		obj.setY(y - canvas.height);
-	  }
-	};
+  // Wrap around world
+  var ensureWithinBounds = function(obj){
+    var x = obj.getX();
+    var y = obj.getY();
+    var prevX = obj.getPreviousX();
+    var prevY = obj.getPreviousY();
+    
+    if(x < 0){
+      obj.setX(prevX + canvas.width);
+      obj.setX(x + canvas.width);
+    }
+    if(x > canvas.width){
+      obj.setX(prevX - canvas.width);
+      obj.setX(x - canvas.width);
+    }
+    if(y < 0){
+      obj.setY(prevY + canvas.height);
+      obj.setY(y + canvas.height);
+    }
+    if(y > canvas.height){
+      obj.setY(prevY - canvas.height);
+      obj.setY(y - canvas.height);
+    }
+  };
 
 
 
-	var handlePlayerInput = function(){
-	  if(!player) player = ships[0];
-	  
-	  if(isPressed(KeyCodes.LEFT)){ player.turnLeft(); }
-	  if(isPressed(KeyCodes.RIGHT)){ player.turnRight(); }
-	  if(isPressed(KeyCodes.UP)){ player.accelerate(); }
-	  if(isPressed(KeyCodes.DOWN)){ player.decelerate(); }
-	  
-	  if(isPressed(KeyCodes.CTRL) && player.canShoot()){
-		var shot = player.shoot();
-		
-		// add shots to game world
-		movable.push(shot);
-		renderable.push(shot);
-	  }
-	};
+  var handlePlayerInput = function(){
+    if(!player) player = ships[0];
+    
+    if(isPressed(KeyCodes.LEFT)){ player.turnLeft(); }
+    if(isPressed(KeyCodes.RIGHT)){ player.turnRight(); }
+    if(isPressed(KeyCodes.UP)){ player.accelerate(); }
+    if(isPressed(KeyCodes.DOWN)){ player.decelerate(); }
+    
+    if(isPressed(KeyCodes.CTRL) && player.canShoot()){
+      var shot = player.shoot();
+      
+      // add shots to game world
+      movable.push(shot);
+      renderable.push(shot);
+    }
+  };
 
 
 
-	/*
-	 Render objects to canvas' context.
-	*/
-	var render = function(){
-	  //Fill Background
-	  context.fillStyle = "#00FFFF";
-	  context.fillRect(0, 0, canvas.width, canvas.height);
-	  
-	  for(var i = 0; i < renderable.length; i++){
-		if(renderable[i]){
-		  renderable[i].render(context);
-		}
-	  }
-	  
-	  context.stroke();
-	};
+  /*
+   Render objects to canvas' context.
+  */
+  var render = function(){
+    //Fill Background
+    context.fillStyle = "#00FFFF";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    
+    for(var i = 0; i < renderable.length; i++){
+      if(renderable[i]){
+        renderable[i].render(context);
+      }
+    }
+    
+    context.stroke();
+  };
 
 
 
-	// Record key press
-	var keydown = function(evt){
-	  var keyCode = (window.event) ? event.keyCode : evt.keyCode;
-	  keysPressed[keyCode] = true;
-	}
+  // Record key press
+  var keydown = function(evt){
+    var keyCode = (window.event) ? event.keyCode : evt.keyCode;
+    keysPressed[keyCode] = true;
+  }
 
 
 
-	// Record key release
-	var keyup = function(evt){
-	  var keyCode = (window.event) ? event.keyCode : evt.keyCode;
-	  keysPressed[keyCode] = false;
-	}
+  // Record key release
+  var keyup = function(evt){
+    var keyCode = (window.event) ? event.keyCode : evt.keyCode;
+    keysPressed[keyCode] = false;
+  }
 
 
 
-	// check whether the given keyCode is pressed
-	var isPressed = function(keyCode){
-	  return keysPressed[keyCode] === true;
-	}
+  // check whether the given keyCode is pressed
+  var isPressed = function(keyCode){
+    return keysPressed[keyCode] === true;
+  }
 
 
 
-	GE.init = init;
-	GE.newGame = newGame;
-	GE.start = start;
-	GE.pause = function(){ gamePaused = true; };
-	GE.setCanvas = setCanvas;
-	GE.isPressed = isPressed;
+  GE.init = init;
+  GE.newGame = newGame;
+  GE.start = start;
+  GE.pause = function(){ gamePaused = true; };
+  GE.setCanvas = setCanvas;
+  GE.isPressed = isPressed;
 
-	return GE;
+  return GE;
 }
--- RenderablePolygon.js
+++ RenderablePolygon.js
@@ -8,164 +8,164 @@ args expects polyCoords.
 polyCoords is an array of [x,y] arrays. Do not repeat the last and first item.
 */
 function RenderablePolygon(movingObj, args){
-	var obj = movingObj || new MovingObject(null, args);
-	var polyCoords = args.polyCoords;
+  var obj = movingObj || new MovingObject(null, args);
+  var polyCoords = args.polyCoords;
 
-	var mobj_equals = obj.equals;
-
-
-
-	/*
-	 Calculates the polygon coordinates for the moving object,
-	 and returns the calculated value.
-	*/
-	var calculatePolygonCoordinates = function(){
-	  // Transform point by movingObj's x, y, viewAngle.
-	  function transformPoint(p){
-		// Rotate, then translate
-		q = Geometry.rotatePoint(p, obj.getViewAngle());
-		q = Geometry.translatePoint(q, obj.getX(), obj.getY());
-		
-		return q;
-	  }
-	  
-	  return polyCoords.map(transformPoint);
-	}
+  var mobj_equals = obj.equals;
 
 
 
-	/*
-	 Returns a box which is the minimal rectangle which contains all
-	 vertices of the polygon.
-	 
-	 Time complexity O(n), by number of points.
-	 
-	 The box is returned in form of [x, y, w, h].
-	*/
-	var calculateBoundingBox = function(){
-	  var coords = calculatePolygonCoordinates();
-	  var minX = coords[0][0], maxX = coords[0][0];
-	  var minY = coords[0][1], maxY = coords[0][1];
-	  
-	  for(var i = 1; i < coords.length; i++){
-		var tx = coords[i][0];
-		var ty = coords[i][1];
-		
-		if(tx < minX){
-		  minX = tx;
-		} else if(tx > maxX){
-		  maxX = tx;
-		}
-		
-		if(ty < minY){
-		  minY = ty;
-		} else if(ty > maxY){
-		  maxY = ty;
-		}
-	  }
-	  
-	  var width = maxX - minX;
-	  var height = maxY - minY;
-	  
-	  return [minX, minY, width, height];
-	}
+  /*
+   Calculates the polygon coordinates for the moving object,
+   and returns the calculated value.
+  */
+  var calculatePolygonCoordinates = function(){
+    // Transform point by movingObj's x, y, viewAngle.
+    function transformPoint(p){
+    // Rotate, then translate
+    q = Geometry.rotatePoint(p, obj.getViewAngle());
+    q = Geometry.translatePoint(q, obj.getX(), obj.getY());
+    
+    return q;
+    }
+    
+    return polyCoords.map(transformPoint);
+  }
 
 
 
-	// Checks whether this collides with another RenderablePolygon
-	// O(mn) time complexity.
-	var collidesWith = function(polyObj){
-	  // Check that the other object has the right methods
-	  if(!polyObj.getBoundingBox || !polyObj.calculatePolygonCoordinates){
-		return false;
-	  }
-	  
-	  // Check whether the bounding boxes overlap before
-	  // calculating whether they intersect.
-	  // O(m + n)
-	  var rectA = obj.getBoundingBox();
-	  var rectB = polyObj.getBoundingBox();
-	  var collisionMightExist = (rectA[0] + rectA[2] >= rectB[0] && rectA[0] <= rectB[0] + rectB[2]) &&
-								(rectA[1] + rectA[3] >= rectB[1] && rectA[1] <= rectB[1] + rectB[3]);
-	  
-	  if(!collisionMightExist){
-		//return false;
-	  }
-	  
-	  // A collision might exist,
-	  // check that it does.
-	  // O(mn)
-	  var poly1 = obj.calculatePolygonCoordinates();
-	  var poly2 = polyObj.calculatePolygonCoordinates();
-	  return Geometry.linePolygonIntersect([[obj.getPreviousX(), obj.getPreviousY()],
-											[obj.getX(), obj.getY()]],
-										   poly2) ||
-			 Geometry.linePolygonIntersect([[polyObj.getPreviousX(), polyObj.getPreviousY()],
-											[polyObj.getX(), polyObj.getY()]],
-										   poly1) ||
-			 Geometry.polygonsIntersect(poly1, poly2);
-	}
+  /*
+   Returns a box which is the minimal rectangle which contains all
+   vertices of the polygon.
+   
+   Time complexity O(n), by number of points.
+   
+   The box is returned in form of [x, y, w, h].
+  */
+  var calculateBoundingBox = function(){
+    var coords = calculatePolygonCoordinates();
+    var minX = coords[0][0], maxX = coords[0][0];
+    var minY = coords[0][1], maxY = coords[0][1];
+    
+    for(var i = 1; i < coords.length; i++){
+      var tx = coords[i][0];
+      var ty = coords[i][1];
+      
+      if(tx < minX){
+        minX = tx;
+      } else if(tx > maxX){
+        maxX = tx;
+      }
+      
+      if(ty < minY){
+        minY = ty;
+      } else if(ty > maxY){
+        maxY = ty;
+      }
+    }
+    
+    var width = maxX - minX;
+    var height = maxY - minY;
+    
+    return [minX, minY, width, height];
+  }
 
 
 
-	var render = function(context){
-	  var x = movingObj.getX();
-	  var y = movingObj.getY();
-	  var va = movingObj.getViewAngle();
-	  
-	  context.beginPath();
-	  
-	  // Angle here, 0 is at EAST, PI/2 is at NORTH.
-	  var coords = calculatePolygonCoordinates();
-	  
-	  context.moveTo(coords[0][0], coords[0][1]);
-	  for(var i = 1; i < polyCoords.length; i++){
-		context.lineTo(coords[i][0], coords[i][1]);
-	  }
-	  context.lineTo(coords[0][0], coords[0][1]);
-	  
-	  context.lineWidth = 2;
-	  context.closePath();
-	  context.stroke();
-	  
-	  //Fill in the shape
-	  //context.fillStyle = "#FF0000";
-	  //context.fill();
-	}
+  // Checks whether this collides with another RenderablePolygon
+  // O(mn) time complexity.
+  var collidesWith = function(polyObj){
+    // Check that the other object has the right methods
+    if(!polyObj.getBoundingBox || !polyObj.calculatePolygonCoordinates){
+      return false;
+    }
+    
+    // Check whether the bounding boxes overlap before
+    // calculating whether they intersect.
+    // O(m + n)
+    var rectA = obj.getBoundingBox();
+    var rectB = polyObj.getBoundingBox();
+    var collisionMightExist = (rectA[0] + rectA[2] >= rectB[0] && rectA[0] <= rectB[0] + rectB[2]) &&
+                              (rectA[1] + rectA[3] >= rectB[1] && rectA[1] <= rectB[1] + rectB[3]);
+    
+    if(!collisionMightExist){
+    //return false;
+    }
+    
+    // A collision might exist,
+    // check that it does.
+    // O(mn)
+    var poly1 = obj.calculatePolygonCoordinates();
+    var poly2 = polyObj.calculatePolygonCoordinates();
+    return  Geometry.linePolygonIntersect([[obj.getPreviousX(), obj.getPreviousY()],
+                      [obj.getX(), obj.getY()]],
+                       poly2) ||
+            Geometry.linePolygonIntersect([[polyObj.getPreviousX(), polyObj.getPreviousY()],
+                           [polyObj.getX(), polyObj.getY()]],
+                            poly1) ||
+            Geometry.polygonsIntersect(poly1, poly2);
+  }
 
 
 
-	var equals = function (o){
-	  // Check mobj equality first.
-	  if(!mobj_equals(o)){
-		return false;
-	  }
-	  
-	  var o_coords = o.getPolygonCoordinates();
-	  
-	  if(polyCoords.length != o_coords.length){ // same number of coords.
-		return false;
-	  }
-	  
-	  // Check all coordinates are the same.
-	  for(var i = 0; i < polyCoords.length; i++){
-		if(Math.abs(o_coords[i][0] - polyCoords[i][0]) > 0.0001 ||
-		   Math.abs(o_coords[i][1] - polyCoords[i][1]) > 0.0001){
-		  return false;
-		}
-	  }
-	  
-	  return true;
-	};
+  var render = function(context){
+    var x = movingObj.getX();
+    var y = movingObj.getY();
+    var va = movingObj.getViewAngle();
+    
+    context.beginPath();
+    
+    // Angle here, 0 is at EAST, PI/2 is at NORTH.
+    var coords = calculatePolygonCoordinates();
+    
+    context.moveTo(coords[0][0], coords[0][1]);
+    for(var i = 1; i < polyCoords.length; i++){
+      context.lineTo(coords[i][0], coords[i][1]);
+    }
+    context.lineTo(coords[0][0], coords[0][1]);
+    
+    context.lineWidth = 2;
+    context.closePath();
+    context.stroke();
+    
+    //Fill in the shape
+    //context.fillStyle = "#FF0000";
+    //context.fill();
+  }
 
 
 
-	obj.calculatePolygonCoordinates = calculatePolygonCoordinates;
-	obj.getPolygonCoordinates = function(){ return polyCoords; };
-	obj.getBoundingBox = calculateBoundingBox;
-	obj.collidesWith = collidesWith;
-	obj.render = render;
-	obj.equals = equals;
+  var equals = function (o){
+    // Check mobj equality first.
+    if(!mobj_equals(o)){
+      return false;
+    }
+    
+    var o_coords = o.getPolygonCoordinates();
+    
+    if(polyCoords.length != o_coords.length){ // same number of coords.
+      return false;
+    }
+    
+    // Check all coordinates are the same.
+    for(var i = 0; i < polyCoords.length; i++){
+      if(Math.abs(o_coords[i][0] - polyCoords[i][0]) > 0.0001 ||
+         Math.abs(o_coords[i][1] - polyCoords[i][1]) > 0.0001){
+        return false;
+      }
+    }
+    
+    return true;
+  };
 
-	return obj;
+
+
+  obj.calculatePolygonCoordinates = calculatePolygonCoordinates;
+  obj.getPolygonCoordinates = function(){ return polyCoords; };
+  obj.getBoundingBox = calculateBoundingBox;
+  obj.collidesWith = collidesWith;
+  obj.render = render;
+  obj.equals = equals;
+
+  return obj;
 }
